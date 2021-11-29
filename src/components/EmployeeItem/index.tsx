b011b6a18b61a0b5cb4a280100f7f8f6e4bf6fb7
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2021-11-29 02:22:42 -0300

    create employee props

---
+++ src/components/EmployeeItem/index.tsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import "./styles.css"
+
+interface Item {
+    id: number;
+    name: string;
+}
+
+interface Employee {
+    id: number;
+    name: string;
+    cpf: string;
+    items: Item[];
+}
+
+interface EmployeeProps {
+    employee: Employee
+}
+const EmployeeItem = ({ employee }: EmployeeProps) => {
+
+    return (
+        <>
+            <section className="employeeContainer">
+                <div className="personalDataEmployee">
+                    <h3>{employee.name}</h3>
+
+                    <div className="field">
+                        <label htmlFor="cpf">
+                            CPF:
+                        </label>
+                        <p id="cpf">{employee.cpf}</p>
+                    </div>
+                </div>
+                <h4>Itens para o café da manhã: </h4>
+                <div className="itemsContainer">
+                    {employee.items.map(item => (
+                        <span className="tag">
+                            <p key={item.id}>
+                                {item.name}
+                            </p>
+                        </span>
+                    ))}
+
+
+                </div>
+            </section >
+
+        </>
+    );
+}
+
+export default EmployeeItem;
--- src/pages/EmployeeList/index.tsx
+++ src/pages/EmployeeList/index.tsx
@@ -1,13 +1,64 @@
-import Employee from "../../components/Employee";
+import { useEffect, useState } from "react";
+import EmployeeItem from "../../components/EmployeeItem";
 import "./styles.css";
 
+interface Item {
+    id: number;
+    name: string;
+}
+interface Employee {
+    id: number;
+    name: string;
+    cpf: string;
+    items: Item[];
+}
 const EmployeeList = () => {
+    const [novoEmployee, setEmployee] = useState({
+        id: 1,
+        name: "João",
+        cpf: "111.111.141.11",
+        items: [
+            {
+                id: 1,
+                name: "Pão"
+            },
+            {
+                id: 2,
+                name: "Salcicha"
+            },
+            {
+                id: 3,
+                name: "Leite"
+            },
+            {
+                id: 4,
+                name: "Café"
+            },
+        ]
+
+    });
+    const [novoEmployee2, setEmployee2] = useState({
+        id: 2,
+        name: "Pedro",
+        cpf: "111.111.141-41",
+        items: [
+            // {
+            //     id: 1,
+            //     name: "Pão"
+            // },
+        ]
+
+    });
+
+
     return (
         <>
-            <Employee />
-            <Employee />
-            <Employee />
-            <Employee />
+            <EmployeeItem employee={novoEmployee2} />
+            <EmployeeItem employee={novoEmployee} />
+            <EmployeeItem employee={novoEmployee} />
+            <EmployeeItem employee={novoEmployee} />
+            <EmployeeItem employee={novoEmployee} />
+
         </>
     );
 }
